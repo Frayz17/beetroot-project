bc9b2625d8a7a784329c2162fc4c340e4ba441b6
frayz <andrydmit@gmail.com>
2019-11-02 17:58:27 +0200

    2 lesson

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class App extends React.Component {
+  state = {
+    displayFlag : true
+  }
+
+  switchDisplay = () => this.setState({
+      displayFlag: !this.state.displayFlag
+    })
+
+  render = () => {
+    return <>
+      <button onClick={this.switchDisplay}>
+        switchDisplay
+      </button>
+      {this.state.displayFlag && 
+        <div>hello</div>}
+    </>
+}
+}
+
+export default App
