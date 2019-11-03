53a96196669c085598ddcef3bd1aace8ad10ae73
Andrey Dmitriev <andrydmit@gmail.com>
2019-11-03 22:34:19 +0200

    added eslintConfig to package.json for fixing unexpected token in standard style plugin

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from 'react'
 
 class App extends React.Component {
   state = {
-    displayFlag : true
+    displayFlag: true
   }
 
   switchDisplay = () => this.setState({
-      displayFlag: !this.state.displayFlag
-    })
+    displayFlag: !this.state.displayFlag
+  })
 
   render = () => {
-    return <>
-      <button onClick={this.switchDisplay}>
+    return (
+      <>
+        <button onClick={this.switchDisplay}>
         switchDisplay
-      </button>
-      {this.state.displayFlag && 
-        <div>hello</div>}
-    </>
-}
+        </button>
+        {this.state.displayFlag &&
+          <div>hello</div>}
+      </>
+    )
+  }
 }
 
 export default App
