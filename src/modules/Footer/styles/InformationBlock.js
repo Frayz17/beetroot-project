f68bad08e989814fac994fbf0d14f6b96bcb271b
Andrey <andrydmit@gmail.com>
2019-12-04 17:49:37 +0200

    responsive design added to footer

--- src/modules/Footer/styles/InformationBlock.js
+++ src/modules/Footer/styles/InformationBlock.js
@@ -4,6 +4,12 @@ import Block from 'components/Block';
 const InformationBlock = styled(Block)`
   display: flex;
   flex-direction: column-reverse;
+
+  @media screen and (min-width: 576px) {
+    flex-direction: row;
+    justify-content: space-around;
+    flex-grow: 2;
+  }
 `;
 
 export default InformationBlock;
--- src/modules/Footer/styles/MenuBlock.js
+++ src/modules/Footer/styles/MenuBlock.js
@@ -3,6 +3,10 @@ import Block from 'components/Block';
 
 const MenuBlock = styled(Block)`
   margin-bottom: 10px;
+
+  @media screen and (min-width: 576px) {
+    flex-grow: 1;
+  }
 `;
 
 export default MenuBlock;
